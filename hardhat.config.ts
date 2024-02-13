9ac0e9475b2bb066fc0061fbb54d1e69d30a1304
wsdt <kevin.riedl@wavect.io>
2024-02-13 18:33:18 +0100

    fix: Tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import '@nomiclabs/hardhat-waffle'
 
 require('dotenv').config()
 
+const LOCAL_PK = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+
 const config: HardhatUserConfig = {
   mocha: {
     timeout: 200000,
@@ -11,15 +13,15 @@ const config: HardhatUserConfig = {
   networks: {
     boba_sepolia: {
       url: 'https://sepolia.boba.network',
-      accounts: [process.env.DEPLOYER_PK],
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
     },
     sepolia: {
       url: 'https://ethereum-sepolia.publicnode.com',
-      accounts: [process.env.DEPLOYER_PK],
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
     },
     boba_goerli: {
       url: 'https://goerli.boba.network',
-      accounts: [process.env.DEPLOYER_PK],
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
     },
     localhost: {
       url: 'http://localhost:9545',
@@ -30,7 +32,12 @@ const config: HardhatUserConfig = {
       ],
     },
   },
-  solidity: '0.8.9',
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: { enabled: true, runs: 10_000 },
+    },
+  },
   paths: {
     sources: "./contracts",
     tests: "./test",
