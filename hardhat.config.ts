ffafa57acde5576ae1dfef300756498a60e6546b
wsdt <kevin.riedl@wavect.io>
2024-02-14 15:58:11 +0100

    feat: Add contract verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,41 @@
 import { HardhatUserConfig } from 'hardhat/types'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import "@nomiclabs/hardhat-etherscan";
 
 require('dotenv').config()
 
 const LOCAL_PK = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 
-const config: HardhatUserConfig = {
+const config: HardhatUserConfig & {etherscan: {apiKey: any, customChains: any}} = {
   mocha: {
     timeout: 200000,
   },
   networks: {
+    mainnet: {
+      url: 'https://eth.llamarpc.com',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
+    bsc: {
+      url: 'https://binance.llamarpc.com',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
+    arbitrumOne: {
+      url: 'https://1rpc.io/arb',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
+    optimisticEthereum: {
+      url: 'https://rpc.optimism.gateway.fm',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
+    boba_eth_mainnet: {
+      url: 'https://boba-ethereum.gateway.tenderly.co',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
+    boba_bnb_mainnet: {
+      url: 'https://gateway.tenderly.co/public/boba-bnb',
+      accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
+    },
     boba_sepolia: {
       url: 'https://sepolia.boba.network',
       accounts: [process.env.DEPLOYER_PK ?? LOCAL_PK],
@@ -44,6 +69,33 @@ const config: HardhatUserConfig = {
     cache: "./cache",
     artifacts: "./artifacts"
   },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      arbitrumOne: "",
+      bsc: "",
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      optimisticEthereum: "",
+    },
+    customChains: [
+      {
+        network: "boba_eth_mainnet",
+        chainId: 288,
+        urls: {
+          apiURL: "",
+          browserURL: "",
+        },
+      },
+      {
+        network: "boba_bnb_mainnet",
+        chainId: 56288,
+        urls: {
+          apiURL: "",
+          browserURL: "",
+        },
+      }
+    ],
+  }
 }
 
 export default config
