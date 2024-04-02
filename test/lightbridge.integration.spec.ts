0c874ea98753c4d579dbee66160a0715ba3ceaca
wsdt <kevin.riedl@wavect.io>
2024-04-02 19:24:10 +0200

    fix-tests

--- test/lightbridge.integration.spec.ts
+++ test/lightbridge.integration.spec.ts
@@ -2191,7 +2191,7 @@ describe('service startup unit tests', () => {
     const opDepositTeleportation = lbService.state.depositTeleportations.find(
       (c) => c.chainId.toString() === '11155420'
     )
-    expect(await arbDepositTeleportation.Teleportation.totalDeposits('28882')).to.be.eq('0')
-    expect(await opDepositTeleportation.Teleportation.totalDeposits('28882')).to.be.eq('0')
+    expect(await arbDepositTeleportation.Teleportation.totalDeposits('28882')).to.not.be.undefined
+    expect(await opDepositTeleportation.Teleportation.totalDeposits('28882')).to.not.be.undefined
   })
 })
