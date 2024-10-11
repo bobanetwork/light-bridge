bce5a646dc2f8ae7f514606a1c9c0e7eea601ff8
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-11 14:33:34 +0530

    formatting & unused import fix

--- src/utils/misc.utils.ts
+++ src/utils/misc.utils.ts
@@ -1,6 +1,3 @@
-import { BigNumber } from 'ethers'
-import { BobaChains } from '@bobanetwork/light-bridge-chains'
-
 export const delay = (ms: number) => {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
